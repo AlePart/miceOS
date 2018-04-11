f3ba7ebce47b31982fcb7cd4df800d35d1297bc1
partesoa <noemail@noemail.no>
2018-04-11 12:18:17 +0200

    moving functions

--- src/kernel.c
+++ src/kernel.c
@@ -106,12 +106,22 @@ void terminal_initialize(void)
 		}
 	}
 }
- 
+
 void terminal_setcolor(uint8_t color) 
 {
 	terminal_color = color;
 }
- 
+void terminal_scroll() 
+{
+  for (size_t y = 0; y < VGA_HEIGHT-1; y++) {
+    for (size_t x = 0; x < VGA_WIDTH; x++) {
+      const size_t index = y * VGA_WIDTH + x;
+      const size_t old_index = (y+1) * VGA_WIDTH + x;
+      terminal_buffer[index] = terminal_buffer[old_index];
+	  }
+  }
+  terminal_row--;
+} 
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) 
 {
 	const size_t index = y * VGA_WIDTH + x;
@@ -139,17 +149,6 @@ void terminal_putchar(char c)
 
 }
 
-void terminal_scroll() 
-{
-  for (size_t y = 0; y < VGA_HEIGHT-1; y++) {
-		for (size_t x = 0; x < VGA_WIDTH; x++) {
-			const size_t index = y * VGA_WIDTH + x;
-      const size_t old_index = (y+1) * VGA_WIDTH + x;
-			terminal_buffer[index] = terminal_buffer[old_index];
-		}
-	}
-  terminal_row--;
-}
 void terminal_write(const char* data, size_t size) 
 {
 	for (size_t i = 0; i < size; i++)
