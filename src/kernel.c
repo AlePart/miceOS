a01a52d8cb77952a5a964a1ca0da5a7f4319431c
Alessandro <AlePart@users.noreply.github.com>
2018-04-11 12:22:30 +0200

    Terminal scroll (#1)
    * term scroll implementation
    
    * moving functions
    
    * oprimization comment

--- src/kernel.c
+++ src/kernel.c
@@ -1,7 +1,8 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
- 
+
+
 /* Check if the compiler thinks we are targeting the wrong operating system. */
 #if defined(__linux__)
 #error "You are not using a cross-compiler, you will most certainly run into trouble"
@@ -49,7 +50,39 @@ size_t strlen(const char* str)
 		len++;
 	return len;
 }
- 
+
+
+char * itoa( int value, char * str, int base )
+{
+    char * rc;
+    char * ptr;
+    char * low;
+    if ( base < 2 || base > 36 )
+    {
+        *str = '\0';
+        return str;
+    }
+    rc = ptr = str;
+    if ( value < 0 && base == 10 )
+    {
+        *ptr++ = '-';
+    }
+    low = ptr;
+    do
+    {
+        *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz"[35 + value % base];
+        value /= base;
+    } while ( value );
+    *ptr-- = '\0';
+    while ( low < ptr )
+    {
+        char tmp = *low;
+        *low++ = *ptr;
+        *ptr-- = tmp;
+    }
+    return rc;
+}
+
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
  
@@ -73,12 +106,23 @@ void terminal_initialize(void)
 		}
 	}
 }
- 
+
 void terminal_setcolor(uint8_t color) 
 {
 	terminal_color = color;
 }
- 
+void terminal_scroll() 
+{
+  // to be optimize
+  for (size_t y = 0; y < VGA_HEIGHT-1; y++) {
+    for (size_t x = 0; x < VGA_WIDTH; x++) {
+      const size_t index = y * VGA_WIDTH + x;
+      const size_t old_index = (y+1) * VGA_WIDTH + x;
+      terminal_buffer[index] = terminal_buffer[old_index];
+	  }
+  }
+  terminal_row--;
+} 
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) 
 {
 	const size_t index = y * VGA_WIDTH + x;
@@ -87,6 +131,7 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
  
 void terminal_putchar(char c) 
 {
+
   if('\n'==c)
   {
     terminal_newline();
@@ -94,23 +139,33 @@ void terminal_putchar(char c)
   else
   {
 	  terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+ 
 	  if (++terminal_column == VGA_WIDTH) {
 		  terminal_column = 0;
 		  if (++terminal_row == VGA_HEIGHT)
-			  terminal_row = 0;
+			  terminal_row--;
 	  }
   }
+
+
 }
- 
+
 void terminal_write(const char* data, size_t size) 
 {
 	for (size_t i = 0; i < size; i++)
+  {
+    if(terminal_row == VGA_HEIGHT)
+     terminal_scroll();
 		terminal_putchar(data[i]);
+    
+  }
 }
  
 void terminal_writestring(const char* data) 
 {
+
 	terminal_write(data, strlen(data));
+
 }
 
 void terminal_newline()
@@ -118,15 +173,24 @@ void terminal_newline()
 	terminal_row++;
 	terminal_column=0;
 }
+
+
 void kernel_main(void) 
 {
 	/* Initialize terminal interface */
 	terminal_initialize();
- 
-	/* Newline support is left as an exercise. */
 	terminal_writestring("Hello, kernel World!");
 	terminal_newline();
 	terminal_writestring("Wow a new Line\n");
-  terminal_writestring("Wow a new Line without call terminal_newline() function");
+  terminal_writestring("Wow a new Line without call terminal_newline() function\n");
+  terminal_writestring("LONG LONG LONG LONG LONG LONG LONG LONG LONG LONG LONG LONG LONG LONG LONG LONG LONG LONG LONG LONG LONG LONG LONG LONG LONG LONG LONG LONG LONG LONG LINE\n");
+  for (int i =0; i< 23; i++)
+  {
+    char n[3];
+    itoa(i,n,10);
+    terminal_writestring(n);
+    terminal_newline();
+  } 
+  terminal_writestring("last line");
 	
 }
