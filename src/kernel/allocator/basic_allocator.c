65cee10b2cda2b19421c1b17766f4be356a62eaa
partesoa <noemail@noemail.no>
2018-04-12 17:12:37 +0200

    basic allocator refactor header.. does not need linked list. Assignament for base_dictionary

--- src/kernel/allocator/basic_allocator.c
+++ src/kernel/allocator/basic_allocator.c
@@ -12,7 +12,7 @@ typedef struct
 {
   uint32_t* base_dictionary;
   uint32_t* type_mask; 
-  ALLOCATOR_LIST* next;
+ 
 }ALLOCATOR_ELEMENT;
 
 typedef enum {
@@ -27,7 +27,7 @@ bool basic_allocator_init(uint32_t* base_address, uint32_t mem_size, PAGE_SIZE p
   uint32_t availble_pages= mem_size>>(uint32_t)pg_size;
   uint32_t allocator_element_needed= (mem_size >>(uint32_t)pg_size);
   uint32_t allocator_mem_reserve= allocator_element_needed*sizeof(ALLOCATOR_ELEMENT) + sizeof(ALLOCATOR_HEADER);
-    
+  uint32_t base_dictionary=allocator_mem_reserve + base_address;
   allocator_hdr_addr = base_address;
   allocator_hdr_addr.sz=pg_size;
   allocator_hdr_addr.spare = 0;
@@ -35,11 +35,11 @@ bool basic_allocator_init(uint32_t* base_address, uint32_t mem_size, PAGE_SIZE p
   ALLOCATOR_ELEMENT* current_el= (ALLOCATOR_ELEMENT*)(allocator_hdr_addr + sizeof(ALLOCATOR_HEADER));
   do
   {
-       current_el->base_dictionary=NULL;
+       current_el->base_dictionary=base_dictionary;
        current_el->type_mask=FREE_PAGE;
-       current_el->next=(current_el+sizeof(ALLOCATOR_ELEMENT));
-       current_el=current_el->next;
+       base_dictionary +=(1<<allocator_hdr_addr.sz);
+ 
   }while(++counter < allocator_reserved_pages);
   
-  current_el->next=NULL; //stopping allocation
+
 }
